bcf57bd3b8a5d574d5850ad82d7dbb2f7f27fff7
Tatiana Berlenko <tatiana.berlenko@gmail.com>
2024-10-18 20:28:40 +0300

    task 01

--- Practice02/src/task01/setup.py
+++ Practice02/src/task01/setup.py
@@ -1,5 +1,6 @@
-from setuptools import find_packages , setup
-
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'task01'
 
@@ -11,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
 
     ],
     install_requires=['setuptools', 'std_msgs'],
